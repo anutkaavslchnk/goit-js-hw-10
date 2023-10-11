a808d3f84df8dfba5e65e4d25cda5da8b605375b
anutkaavslchnk <anutochkaaaaa14@gmail.com>
2023-10-11 11:47:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const slimSelect = new SlimSelect({
       data: mapData,
       select: selectBreed,
+  
       events: {
         afterChange: (info) => {
           const selectId = info[0].value;
@@ -27,18 +28,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
           fetchCatByBreed(selectId)
             .then(function (catData) {
-              console.log('Cat data:', catData);
+           
               divMarkup.innerHTML = `<img src="${catData.url}" alt="${catData.name}" class="breed-icon" width="500" height="auto" />
                   <h2 class="name-breed">${catData.name}</h2>
                   <p class="desc-breed">${catData.description}</p>
                   <p class="personality-breed"><span style="font-weight:700">Temperament: </span>${catData.temperament}</p>
                 `;
-               
+        
               loader.style.display = 'none';
               divMarkup.style.display = 'block';
             })
             .catch((err) => {
-              console.error('Fetch Cat Error:', err);
+              
               Notiflix.Notify.failure('Oops! Something went wrong with fetching cat data! Try reloading the page!');
               loader.style.display = 'none';
               error.style.display = 'block';
@@ -61,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
       initializationSelect();
     })
     .catch((err) => {
-      console.error('Fetch Breeds Error:', err);
+      
       Notiflix.Notify.failure('Oops! Something went wrong with fetching breeds! Try reloading the page!');
       loader.style.display = 'none';
       error.style.display = 'block';
